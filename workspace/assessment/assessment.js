c9c0e9f04e1bfff43099004a8a4771431f2b1a67
yujiro-baba <babajohnyujiro@gmail.com>
2020-11-19 01:34:11 +0900

    chapter18完了

--- workspace/assessment/assessment.js
+++ workspace/assessment/assessment.js
@@ -32,6 +32,22 @@ assessmentButton.onclick = ()=> {
 	resultDivided.appendChild(paragraph);
 
 	//TODO　ツイートエリアの作成
+	removeAllChildren(tweetDivided);
+	const anchor = document.createElement('a');
+	const hrefValue =
+		'https://twitter.com/intent/tweet?button_hashtag=' +
+		encodeURIComponent('あなたのいいところ') +
+		'&ref_src=twsrc%5Etfw';
+	anchor.setAttribute('href', hrefValue);
+	anchor.className = 'twitter-hashtag-button';
+	anchor.setAttribute('data-text', '診断結果の文章');
+	anchor.innerText = 'Tweet #あなたのいいところ';
+	tweetDivided.appendChild(anchor);
+
+	// widgets.js の設定
+	const script = document.createElement('script');
+	script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
+	tweetDivided.appendChild(script);
 }
 
 const answers = [
