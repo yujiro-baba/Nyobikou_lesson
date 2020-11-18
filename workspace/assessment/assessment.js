486dd4fc4ded18e77b8676bcc842aac1d93ac80f
yujiro-baba <babajohnyujiro@gmail.com>
2020-11-18 17:47:14 +0900

    chapter17完了

--- workspace/assessment/assessment.js
+++ workspace/assessment/assessment.js
@@ -1,4 +1,39 @@
 'use strict';
+const userNameInput = document.getElementById('user-name');
+const assessmentButton = document.getElementById('assessment');
+const resultDivided = document.getElementById('result-area');
+const tweetDivided = document.getElementById('tweet-area');
+
+/**
+ * 指定した要素の子どもを全て削除する
+ * @param {HTMLElement} element HTMLの要素
+ */
+function removeAllChildren(element) {
+	while(element.firstChild){
+		//子どもの要素があるかぎり削除
+		element.removeChild(element.firstChild);
+	}
+}
+assessmentButton.onclick = ()=> {
+	const userName = userNameInput.value;	
+	if(userName.length === 0) {
+		//名前が空のときは処理を終了する
+		return;
+	}
+	//診断結果表示エリアの作成
+	removeAllChildren(resultDivided);
+	const header = document.createElement('h3');
+	header.innerText = '診断結果';
+	resultDivided.appendChild(header);
+
+	const paragraph = document.createElement('p');
+	const result = assessment(userName);
+	paragraph.innerText = result;
+	resultDivided.appendChild(paragraph);
+
+	//TODO　ツイートエリアの作成
+}
+
 const answers = [
 	'{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
 	'{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
@@ -38,7 +73,7 @@ function assessment(userName) {
 	return result;
 }
 
-console.assert(
+/*console.assert(
 	assessment('太郎') ===
 	'太郎のいいところは決断力です。次郎がする決断にいつも助けられる人がいます。',
 	'診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
@@ -47,5 +82,5 @@ console.assert(
 console.assert(
 	assessment('太郎') === assessment('太郎'),
 	'入力が同じ名前なら同じ診断結果を出力する処理が正しくありません。'
-);
+);*/
 
